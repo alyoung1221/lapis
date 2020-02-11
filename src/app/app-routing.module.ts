c641c917ff6ec04c05a8169765b069dfee97adbf
ericmahoney <eric.mahoney22@gmail.com>
2020-02-11 00:38:12 -0500

    added navbar with routes to branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AppComponent},
+  { path: 'contact', component: AppComponent},
+  { path: 'profile', component: AppComponent},
+  { path: 'home', component: AppComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
