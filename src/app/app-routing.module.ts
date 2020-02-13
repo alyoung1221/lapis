94158a2adf97d8194a759b62dcf756f99e57c9d4
ericmahoney <eric.mahoney22@gmail.com>
2020-02-13 13:17:57 -0500

    created profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 const routes: Routes = [
   { path: 'about', component: AppComponent},
   { path: 'contact', component: AppComponent},
-  { path: 'profile', component: AppComponent},
+  { path: 'profile', component: ProfileComponent},
   { path: 'home', component: AppComponent},
 ];
 
