9af23c5b1008fda151c02f338278f7d1df611433
ericmahoney <eric.mahoney22@gmail.com>
2020-02-13 13:30:54 -0500

    added sample component for testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SampleComponent } from './sample/sample.component';
 
 
 const routes: Routes = [
-  { path: 'about', component: AppComponent},
-  { path: 'contact', component: AppComponent},
+  { path: 'about', component: SampleComponent},
+  { path: 'contact', component: SampleComponent},
   { path: 'profile', component: ProfileComponent},
-  { path: 'home', component: AppComponent},
+  { path: '', component: SampleComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProfileComponent,
+    SampleComponent,
   ],
   imports: [
     BrowserModule,
