cfd6051fd8bbc821d72d34bf54e73c5bde9b9d82
ericmahoney <eric.mahoney22@gmail.com>
2020-02-14 16:27:55 -0500

    added a shared component called usercard

---
+++ src/app/shared/usercard/usercard.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from 'firebase';
+import { ProfileComponent } from 'src/app/profile/profile.component';
+
+@Component({
+  selector: 'app-usercard',
+  templateUrl: './usercard.component.html',
+  styleUrls: ['./usercard.component.css']
+})
+export class UsercardComponent implements OnInit {
+  @Input() user: ProfileComponent;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
