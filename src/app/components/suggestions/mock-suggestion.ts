58eb9b4204300edd991dd53bdc129d87dee83220
dlee100 <danieljplee1@gmail.com>
2020-02-29 22:43:47 -0500

    added more elements to the Suggestion[] array

--- src/app/components/suggestions/mock-suggestion.ts
+++ src/app/components/suggestions/mock-suggestion.ts
@@ -1,14 +1,14 @@
 import { Suggestion } from 'src/app/components/suggestions/suggestion';
 
 export const SUGGESTIONS: Suggestion[] = [
-  { id: 11, name: 'James Holden'},
-  { id: 12, name: 'Amos Burton' },
-  { id: 13, name: 'Naomi Nagata' },
-  { id: 14, name: 'Alex Kamal' },
-  { id: 15, name: 'Joe Miller' },
-  { id: 16, name: 'Camina Drummer' },
-  { id: 17, name: 'Prax Meng' },
-  { id: 18, name: 'Julie Mao' },
-  { id: 19, name: 'Roberta Draper' },
-  { id: 20, name: 'Chrisjen Avasarala' }
+  { id: 1, name: 'James Holden', hobbies: 'Drinking coffee', interests: 'Doing the right thing'},
+  { id: 2, name: 'Amos Burton', hobbies: 'Drinking and fighting', interests: 'Maintaining'},
+  { id: 3, name: 'Naomi Nagata', hobbies: 'Exploring', interests: 'Engineering' },
+  { id: 4, name: 'Alex Kamal', hobbies: 'Listening to country', interests: 'Flying'},
+  { id: 5, name: 'Joe Miller', hobbies: 'Stalking', interests: 'Investigating' },
+  { id: 6, name: 'Camina Drummer', hobbies: 'Wall ball, drinking', interests: 'Being tactile' },
+  { id: 7, name: 'Prax Meng', hobbies: 'Planting plants', interests: 'Plants' },
+  { id: 8, name: 'Julie Mao', hobbies: 'Racing, social activist', interests: 'Liberation'},
+  { id: 9, name: 'Roberta Draper', hobbies: 'Drinking and fighting', interests: 'Integrity'},
+  { id: 10, name: 'Chrisjen Avasarala', hobbies: 'Cursing, roasting people', interests: 'Being vulgar' }
 ];
--- src/app/components/suggestions/suggestion.ts
+++ src/app/components/suggestions/suggestion.ts
@@ -1,4 +1,6 @@
 export interface Suggestion {
     id: number;
     name: string;
+    hobbies: string;
+    interests: string;
   }
