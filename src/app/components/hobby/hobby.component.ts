d02d0100833926b4125cd1d9cdd28f7e655bd767
Alana Young <ayoung39@masonlive.gmu.edu>
2020-05-19 15:08:21 -0400

    Added title for hobby page

--- src/app/components/hobby/hobby.component.ts
+++ src/app/components/hobby/hobby.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HobbiesService } from '../../services/hobbies.service';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-hobby',
@@ -13,12 +14,14 @@ export class HobbyComponent implements OnInit {
   userList = [];
   constructor(
     private route: ActivatedRoute,
-    private hobbies: HobbiesService
+    private hobbies: HobbiesService,
+    public app: AppComponent
     ) {}
 
   ngOnInit() {
-    this.urlParam = this.route.snapshot.params.hobby.toLowerCase();
-    this.getUserList(this.urlParam);
+    this.urlParam = this.route.snapshot.params.hobby;
+    this.app.setTitle(this.urlParam);
+    this.getUserList(this.urlParam.toLowerCase());
   }
   getUserList(urlParam) {
     this.userList = this.hobbies.getHobbies(urlParam);
